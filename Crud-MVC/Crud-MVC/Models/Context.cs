45dcf22ca0f1448ef9ab8a3e41761e51ef7221bb
Leandro-SI <llmanutencao@bol.com.br>
2021-08-07 22:58:18 -0300

    projeto inicial

---
+++ Crud-MVC/Crud-MVC/Models/Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crud_MVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-6CICVVM\sqlexpress;Database=Crudmvc;Integrated Security=True");
+        }
+    }
+}
